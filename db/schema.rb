99cf75a0e805d3a2b454b2aac804f1a7b8a7c8a1
ueda-kenta <kosian.4141@gmail.com>
2020-10-02 06:23:08 +0000

    [add]user_controller,post_controller[edit]devise_view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_041956) do
+ActiveRecord::Schema.define(version: 2020_10_02_060651) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2020_10_02_041956) do
     t.integer "user_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
