3a012da7773f4b1a284f62018874d7e06e22871d
ueda-kenta <kosian.4141@gmail.com>
2020-10-05 01:41:23 +0000

    [add]like

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,15 @@ class Post < ApplicationRecord
    accepts_nested_attributes_for :sake_brewery #１つのform_forでsake_breweryモデルも保存するため
   # belongs_to :tag
   belongs_to :user
+   has_many :likes, dependent: :destroy
 
   attachment :sake_img
   validates :sake_name, presence: true, length: { maximum: 20 }
   validates :sake_text, presence: true, length: { maximum: 140 }
   # validates :sake_img_id, presence: true
+
+  def liked_by?(user)
+  	likes.where(user_id: user.id).exists?
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+   has_many :likes, dependent: :destroy
   attachment :profile_img
 
   validates :last_name, presence: true, length: { maximum: 8 }
