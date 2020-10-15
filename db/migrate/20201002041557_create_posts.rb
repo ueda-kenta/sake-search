d18875de75c5e19e102b4f49313dede29534dccd
ueda-kenta <kosian.4141@gmail.com>
2020-10-15 13:16:18 +0000

    [fix]midration

--- db/migrate/20201002041557_create_posts.rb
+++ db/migrate/20201002041557_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.integer :sake_brewery_id
       t.string :sake_name
       t.string :sake_img_id
-      t.integer :tag_id
+      # t.integer :tag_id
       t.text :sake_text
       t.integer :sake_degree
 
--- db/migrate/20201007052420_remove_tag_from_posts.rb
+++ db/migrate/20201007052420_remove_tag_from_posts.rb
@@ -1,5 +1,5 @@
 class RemoveTagFromPosts < ActiveRecord::Migration[5.2]
   def change
-    remove_integer :posts, :tag_id
+    # remove_integer :posts, :tag_id
   end
 end
