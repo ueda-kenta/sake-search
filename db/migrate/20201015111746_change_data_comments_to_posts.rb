3ea95e8e4562750778e27241ac2c889f29bb7631
ueda-kenta <kosian.4141@gmail.com>
2020-10-15 11:28:30 +0000

    [change]reference_to_integer

---
+++ db/migrate/20201015111746_change_data_comments_to_posts.rb
@@ -0,0 +1,14 @@
+class ChangeDataCommentsToPosts < ActiveRecord::Migration[5.2]
+  def change
+  	change_column :comments, :post_id, :integer
+  	change_column :likes, :post_id, :integer
+  	change_column :likes, :user_id, :integer
+  	change_column :posts, :sake_brewery_id, :integer
+  	change_column :posts, :user_id, :integer
+  	change_column :relationships, :user_id, :integer
+  	change_column :relationships, :follow_id, :integer
+  	change_column :relationships, :user_id, :integer
+  	change_column :tag_posts, :tag_id, :integer
+  	change_column :tag_posts, :post_id, :integer
+  end
+end
