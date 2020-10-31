07294858ac38b971007f5cbd84f3078c93d6483c
ueda-kenta <kosian.4141@gmail.com>
2020-10-31 06:27:42 +0000

    [fix]comment_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comments = Comment.where(post_id: @post.id)
     comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
-    if user == current_user
+    if comment.user == current_user
       comment.destroy
     else
       redirect_back(fallback_location: posts_path)
